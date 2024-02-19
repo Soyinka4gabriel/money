485253cef0aae09ffccc764dc283c702a982c040
Soyinka4gabriel <142997790+Soyinka4gabriel@users.noreply.github.com>
2024-02-19 13:51:10 +0100

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from cgitb import text
+from tkinter import dialog
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import Screen
@@ -5,34 +7,33 @@ from kivy.uix.screenmanager import ScreenManager
 from kivy.core.window import Window
 from kivymd.uix.dialog import MDDialog
 from kivy.clock import  Clock
+
 Window.size=(320,600)
 class MainWid(Screen):
     pass
        
-class MoneyApp(MDApp):
+class MoveByApp(MDApp):
     
     def build(self): 
         self.icon = "moveby.png"
-        self.theme_cls.primary_palette ="Blue"
+        self.theme_cls.primary_palette ="Green"
         self.theme_cls.primary_hue = "700"
-        self.theme_cls.theme_style="Light"
+        self.theme_cls.theme_style="Dark"
         global screen_manager
         screen_manager = ScreenManager()
-        screen_manager.add_widget(Builder.load_file("welcome.kv"))
+        screen_manager.add_widget(Builder.load_file("moveby.kv"))
         screen_manager.add_widget(Builder.load_file("login.kv"))
-        screen_manager.add_widget(Builder.load_file("dashboard.kv"))
+        screen_manager.add_widget(Builder.load_file("check.kv"))
+        
+        
+
         return screen_manager
-    
     def on_start(self):
-        Clock.schedule_once(self.login, 3)
-        screen_manager.current = "welcome"
+         Clock.schedule_once(self.login, 2)
+         screen_manager.current = "welcome"
     def login(self, *args):
         screen_manager.current = "login"  
-    def dash(self):
-        screen_manager.current = "dashboard"   
-    def show(self):
-        dialog = MDDialog(type="simple",
-                        text="Congratulation\nYou have successfully apply for your loan chech Email for more details")
-        dialog.open() 
+    def car(self):
+        screen_manager.current = "check"    
         
-MoneyApp().run()       
+MoveByApp().run()       
